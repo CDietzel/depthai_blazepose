ce9669a6fc81fc465ae6a9bc012618f89ff169d1
CDietzel <amothwo@gmail.com>
2022-05-13 02:28:49 -0400

    GAH, CODE IS DONE!

--- runner.py
+++ runner.py
@@ -1,20 +1,22 @@
 import marshal
 import pickle
 import sys
-from math import cos, sin
+from math import cos, sin, sqrt
 from pathlib import Path
 from string import Template
-from math import sqrt
 
 import cv2
 import depthai as dai
+import matplotlib.pyplot as plt
 import numpy as np
 from numpy.core.fromnumeric import trace
+from BlazeposeRenderer import BlazeposeRenderer
 
 import mediapipe_utils as mpu
 from FPS import FPS, now
 from o3d_utils import Visu3D
 from RLSEstimator import RLSEstimator
+from BlazeposeRenderer import LINES_BODY
 
 
 class PoseEstimatorRunner:
@@ -65,25 +67,31 @@ class PoseEstimatorRunner:
         return estimates
 
     def refine_keypoints(self, keypoints_arr, estimates_arr, skeleton, pairs):
-        refined_keypoints = keypoints_arr
-        for i, (keypoints, estimates) in enumerate(zip(keypoints_arr, estimates_arr)):
-            for pair in pairs:
-                quad = np.take(skeleton, pair, axis=0)
-                mid = np.bincount(quad.flatten()).argmax()
-                lr = quad[quad != mid]
-                left = lr[0]
-                right = lr[1]
-                A = keypoints[left]
-                B = keypoints[right]
-                C = keypoints[mid]
-                dA = estimates[pair[0]]
-                dB = estimates[pair[1]]
-                newC = self._refine_distance(A, B, C, dA, dB)
-                refined_keypoints[i][mid] = newC
+        refined_keypoints = keypoints_arr.copy()
+        for iter in range(5):  # maybe it will converge?
+            for i, (keypoints, estimates) in enumerate(
+                zip(keypoints_arr, estimates_arr)
+            ):
+                for pair in pairs:
+                    quad = np.take(skeleton, pair, axis=0)
+                    mid = np.bincount(quad.flatten()).argmax()
+                    lr = quad[quad != mid]
+                    left = lr[0]
+                    right = lr[1]
+                    A = keypoints[left]
+                    B = keypoints[right]
+                    C = keypoints[mid]
+                    dA = estimates[pair[0]]
+                    dB = estimates[pair[1]]
+                    newC = self._refine_distance(A, B, C, dA, dB)
+                    refined_keypoints[i][mid] = newC
         return refined_keypoints
 
     def _refine_distance(self, A, B, C, dAC, dBC):
         dAB = np.linalg.norm(A - B)
+        if dAB > dAC + dBC:  # Escape hatch for when geometrically
+            # dAB = dAC + dBC  # impossible constraints happen
+            return C
         AB = B - A
         AB = AB / np.linalg.norm(AB)  # x axis
         AC = C - A
@@ -116,7 +124,7 @@ class PoseEstimatorRunner:
         C2D = (M @ Caug)[0:2]
         num = (-(dBC**2)) + (dAB**2) + (dAC**2)
         new_Cx = num / (2 * dAB)
-        new_Cy = sqrt((dAC**2) - (new_Cx**2))
+        new_Cy = sqrt(abs((dAC**2) - (new_Cx**2)))
         if C2D[1] < 0:
             new_Cy = -new_Cy
         new_C2D = np.array([new_Cx, new_Cy, 0, 1])
@@ -127,47 +135,124 @@ class PoseEstimatorRunner:
 
 def main():
     runner = PoseEstimatorRunner()
-    poses_path = "/home/locobot/Documents/Repos/depthai_blazepose/outputs/moving_no_smoothing.pickle"
-    skeleton = (
-        (11, 13),
-        (12, 14),
-        (13, 15),
-        (14, 16),
-        (23, 25),
-        (24, 26),
-        (25, 27),
-        (26, 28),
-        (27, 29),
-        (28, 30),
-        (27, 31),
-        (28, 32),
-        (29, 31),
-        (30, 32),
-    )
+    poses_path = "/home/locobot/Documents/Repos/depthai_blazepose/outputs/no_moving_no_smoothing.pickle"
+    # skeleton = (
+    #     (11, 13),
+    #     (12, 14),
+    #     (13, 15),
+    #     (14, 16),
+    #     (23, 25),
+    #     (24, 26),
+    #     (25, 27),
+    #     (26, 28),
+    #     (27, 29),
+    #     (28, 30),
+    #     (27, 31),
+    #     (28, 32),
+    #     (29, 31),
+    #     (30, 32),
+    # )
+    skeleton = LINES_BODY
+    # pairs = (
+    #     (0, 2),
+    #     (1, 3),
+    #     (4, 6),
+    #     (5, 7),
+    #     (6, 8),
+    #     (6, 10),
+    #     (7, 9),
+    #     (7, 11),
+    #     (8, 12),
+    #     (9, 13),
+    #     (10, 12),
+    #     (11, 13),
+    # )
     pairs = (
-        (0, 2),
-        (1, 3),
-        (4, 6),
-        (5, 7),
-        (6, 8),
-        (6, 10),
-        (7, 9),
-        (7, 11),
+        (8, 9),
         (8, 12),
-        (9, 13),
-        (10, 12),
-        (11, 13),
+        (9, 12),
+        (3, 8),
+        (3, 11),
+        (8, 11),
+        (12, 13),
+        (3, 4),
+        (13, 14),
+        (13, 16),
+        (14, 16),
+        (4, 5),
+        (4, 7),
+        (5, 7),
+        (15, 16),
+        (6, 7),
+        (14, 15),
+        (5, 6),
+        (9, 10),
+        (9, 20),
+        (10, 20),
+        (10, 11),
+        (10, 17),
+        (11, 17),
+        (20, 21),
+        (17, 18),
     )
-    poses = runner.load_poses(poses_path, 20, 80)
+    poses = runner.load_poses(poses_path, 10, 200)
     keypoints = runner.extract_keypoints(poses)
     distances = runner.calc_distances(keypoints, skeleton)
     estimates = runner.estimate_distances(distances)
     refined_keypoints = runner.refine_keypoints(keypoints, estimates, skeleton, pairs)
 
-    
+    r_forearm_dist = distances[:, 4]
+    r_forearm_est = estimates[:, 4]
 
+    r_shoulder_key_orig = keypoints[:, 12]
+    r_shoulder_key_ref = refined_keypoints[:, 12]
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = plt.axes(projection="3d")
+    fig.set_tight_layout(True)
+    ax.scatter(r_shoulder_key_orig[:, 0], r_shoulder_key_orig[:, 1], r_shoulder_key_orig[:, 2])
+    x = ax.get_xlim()
+    y = ax.get_ylim()
+    z = ax.get_zlim()
+    ax.set_xlabel("X-axis", fontweight="bold")
+    ax.set_ylabel("Y-axis", fontweight="bold")
+    ax.set_zlabel("Z-axis", fontweight="bold")
+    ax.set_title("Initial Right Shoulder Locations (m)")
+    ax.view_init(elev=0, azim=0)
+    plt.savefig('1.png', bbox_inches='tight')
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = plt.axes(projection="3d")
+    fig.set_tight_layout(True)
+    ax.scatter(r_shoulder_key_ref[:, 0], r_shoulder_key_ref[:, 1], r_shoulder_key_ref[:, 2])
+    ax.set_xlim(x)
+    ax.set_ylim(y)
+    ax.set_zlim(z)
+    ax.set_xlabel("X-axis", fontweight="bold")
+    ax.set_ylabel("Y-axis", fontweight="bold")
+    ax.set_zlabel("Z-axis", fontweight="bold")
+    ax.set_title("Refined Right Shoulder Locations (m)")
+    ax.view_init(elev=0, azim=0)
+    plt.savefig('2.png', bbox_inches='tight')
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = plt.axes()
+    ax.set_title("Measured Right Forearm Lengths (m)")
+    plt.plot(r_forearm_dist)
+    plt.savefig('3.png', bbox_inches='tight')
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = plt.axes()
+    ax.set_title("Estimated Right Forearm Lengths (m)")
+    plt.plot(r_forearm_est)
+    plt.savefig('4.png', bbox_inches='tight')
+
+    print("Original keypoint std: " + str(np.std(r_shoulder_key_orig, axis=0)))
+    print("Refined keypoint std:  " + str(np.std(r_shoulder_key_ref, axis=0)))
+
+    plt.show()
+    pass
 
 
 if __name__ == "__main__":
     main()
-
